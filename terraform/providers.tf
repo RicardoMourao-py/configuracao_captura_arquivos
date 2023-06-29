655b32961a90b8baab4a17edf0304f3b87512a8e
Ricardo Mourão <72896483+RicardoMourao-py@users.noreply.github.com>
2023-06-29 17:51:18 -0300

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "gcs" {}
+
+  required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.44.1"
+    }
+  }
+}
